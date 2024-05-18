24562a8df9b325571815d322de30839ad0ce7491
k3v-5 <142853881+k3v-5@users.noreply.github.com>
2024-05-18 14:02:43 -0600

    Inicializacion interfaz e integracion backend

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,15 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <Horario />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import Horario from "./components/Horario.vue";
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    Horario,
+  },
+};
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+<style></style>
